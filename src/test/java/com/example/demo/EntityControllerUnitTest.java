b2e4b13a0af082cb8449953a5f20e5a1d63ac22f
Jan Domenech <145030629+jandomenenech@users.noreply.github.com>
2024-02-27 13:16:22 +0100

    Update EntityControllerUnitTest.java

--- src/test/java/com/example/demo/EntityControllerUnitTest.java
+++ src/test/java/com/example/demo/EntityControllerUnitTest.java
@@ -46,17 +46,52 @@ class DoctorControllerUnitTest{
     @MockBean
     private DoctorRepository doctorRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+        //Miramos si salta el status de not found al buscar un id no existente de doctores
+    void shouldNotGetDoctor() throws Exception{
+        int id = 5;
+        mockMvc.perform(get("/api/doctors/"+ id))
+                .andExpect(status().isNotFound());
     }
+    @Test
+        //Comprobamos si salta la excepecion de no content si devuelve un getAll vacio
+    void shouldGetEmptyDoctor() throws Exception {
+        mockMvc.perform(get("/api/doctors"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+        //Miramos si salta la excepcion isCreated al crear bien un doctor
+    void shouldCreateDoctor() throws Exception {
+
+        Doctor doctor = new Doctor("Jose","Fernandez",39,"josefernandez@gmail.com");
+        mockMvc.perform(post("/api/doctor").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(doctor)))
+                .andExpect(status().isCreated());
+    }
+
+
+    @Test
+        //Testeamos si salta un ok al borrar todos los doctores
+    void shouldDeleteAllDoctors() throws Exception{
+        mockMvc.perform(delete("/api/doctors"))
+                .andExpect(status().isOk());
+
+    }
+    @Test
+        //Comprobamos si salta el not found al intentar borrar un doctor no existente
+    void shouldNotDeleteDoctor() throws Exception {
+        long id = 23;
+        mockMvc.perform(delete("/api/doctors/" + id))
+                .andExpect(status().isNotFound());
+    }
+
 }
 
 
@@ -66,17 +101,54 @@ class PatientControllerUnitTest{
     @MockBean
     private PatientRepository patientRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+        //Miramos si salta el status de not found al buscar un id no existente de pacientes
+    void shouldNotGetPatient() throws Exception{
+        int id = 5;
+        mockMvc.perform(get("/api/patients/"+ id))
+                .andExpect(status().isNotFound());
     }
+
+    @Test
+        //Comprobamos si salta la excepecion de no content si devuelve un getAll vacio
+    void shouldGetEmptyPatient() throws Exception {
+        mockMvc.perform(get("/api/patients"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+        //Miramos si salta la excepcion isCreated al crear bien un doctor
+    void shouldCreatePatient() throws Exception {
+
+        Patient patient = new Patient("Manuel","Perez",20,"manuelperez@gmail.com");
+        mockMvc.perform(post("/api/patient").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(patient)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+        //Testeamos si salta un ok al borrar todos los pacientes
+    void shouldNotDeletePatient() throws Exception {
+        long id = 50;
+        mockMvc.perform(delete("/api/patients/" + id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+        //Comprobamos si salta el not found al intentar borrar un paciente no existente
+    void shouldDeleteAllPatients() throws Exception{
+        mockMvc.perform(delete("/api/patients"))
+                .andExpect(status().isOk());
+
+    }
+
+
 
 }
 
@@ -86,16 +158,50 @@ class RoomControllerUnitTest{
     @MockBean
     private RoomRepository roomRepository;
 
-    @Autowired 
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void this_is_a_test(){
-        // DELETE ME
-        assertThat(true).isEqualTo(false);
+        //Miramos si salta el status de not found al buscar un id no existente de sala
+    void shouldNotGetRoom() throws Exception{
+        int id = 5;
+        mockMvc.perform(get("/api/rooms/"+ id))
+                .andExpect(status().isNotFound());
     }
 
+    @Test
+        //Comprobamos si salta la excepecion de no content si devuelve un getAll vacio
+    void shouldGetEmptyRooms() throws Exception {
+        mockMvc.perform(get("/api/rooms"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+        //Miramos si salta la excepcion isCreated al crear bien un sala
+    void shouldCreateRoom() throws Exception {
+
+        Room room = new Room("Dermatologia");
+        mockMvc.perform(post("/api/room").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(room)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+        //Testeamos si salta un ok al borrar todos los salas
+    void shouldNotDeleteRoom() throws Exception {
+        long id = 2;
+        mockMvc.perform(delete("/api/rooms/" + id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+        //Comprobamos si salta el not found al intentar borrar un sala no existente
+    void shouldDeleteAllRooms() throws Exception{
+        mockMvc.perform(delete("/api/rooms"))
+                .andExpect(status().isOk());
+
+    }
 }
